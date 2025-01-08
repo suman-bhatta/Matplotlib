6c606d6311b3564f008334401d2d4a6c332d5711
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-08 20:51:40 +0545

    Enhance plotting examples with additional functionalities and visualizations using matplotlib

--- plotting.py
+++ plotting.py
@@ -1,8 +1,45 @@
-import matplotlib,pyplot as plt 
+"Plotting x and y points"
+
+import matplotlib.pyplot as plt 
 import numpy as np 
 
 xpoints = np.array([1, 8])
 ypoints = np.array([3, 10])
 
 plt.plot(xpoints, ypoints)
+plt.show()
+
+
+"Plotting Without Line"
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
+
+
+"Multiple Points"
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+
+"Default X-Points"
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10, 5, 7])
+
+plt.plot(ypoints)
 plt.show()
